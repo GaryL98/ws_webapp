d74f88578b888ae10603983567aab195b1bbbae3
Gary Leung <garyleunguk@outlook.com>
2019-04-13 14:55:01 +0100

    fullscreen toggle added

--- webpages/api.js
+++ webpages/api.js
@@ -11,4 +11,57 @@ function carousel() {
   if (index > x.length) {index = 1}
   x[index-1].style.display = "block";
   setTimeout(carousel, 5000); // Change image every 5 seconds
-}
+};
+
+function toggleFullscreen(elem) {
+  elem = elem || document.documentElement;
+  if (!document.fullscreenElement && !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+};
+
+function toggleHint() {
+  var x = document.getElementById("hint");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+};
+
+function toggleConfigureBtn() {
+  var x = document.getElementById("configure");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+};
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 70) {
+    toggleFullscreen();
+    toggleHint();
+    toggleConfigureBtn();
+  }
+};
