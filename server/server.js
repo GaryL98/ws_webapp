c46675c7efb6670af46a9d58c970d632f1f7fdc6
Gary Leung <garyleunguk@outlook.com>
2019-05-05 15:29:13 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -2,8 +2,49 @@
 
 const fs = require('fs');
 const express = require('express');
+const multer = require('multer'); // file storing middleware
+const bodyParser = require('body-parser'); //cleans our req.body
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false})); //handle body requests
+app.use(bodyParser.json()); // let's make JSON work too!
+
+const multerConfig = {
+  storage: multer.diskStorage({
+    //Setup where the user's file will go
+    destination: function(req, file, next){
+      next(null, './webpages/images');
+    },
+
+    //Then give the file a unique name
+    filename: function(req, file, next){
+      console.log(file);
+      const ext = file.mimetype.split('/')[1];
+      next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+    }
+  }),
+
+  //A means of ensuring only images are uploaded.
+  fileFilter: function(req, file, next){
+    if(!file){
+      next();
+    }
+    const image = file.mimetype.startsWith('image/');
+    if(image){
+      console.log('photo uploaded');
+      next(null, true);
+    }else{
+      console.log("file not supported");
+      //TODO:  A better message response to user on failure.
+      return next();
+    }
+  }
+};
+
+app.post('/upload',multer(multerConfig).single('photo'),function(req,res){
+  res.send('Complete!');
+});
 
 app.get('/save', (req, res) => {
   const shopName = (req.query.shopName);
